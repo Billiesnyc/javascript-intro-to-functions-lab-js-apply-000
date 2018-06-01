bfd54eeef3000f1b6208381eacd492a29feddc2e
Billie Simmons <billiesnyc@gmail.com>
2018-06-01 19:50:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string)
 }
 
 function sayHiToGrandma(string)
-{ if (string 
+{ if (string === string
   lowercase.toLowerCase() === lowercase
   return "I can\'t hear you!"
   }
